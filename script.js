ed635a4f4afb6412c3ca2cb5ef14af52a98e2716
Junlys3 <125740363+Junlys3@users.noreply.github.com>
2023-11-25 15:52:12 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let menulist = window.document.querySelector(".menu-list")
+
+
+
+function entrou(){
+    
+    menulist.style.transform = 'translateX(0)'
+
+}
+
+function saiu(){
+    menulist.style.transform = 'translateX(100%)'
+}
+
