25eec23284b4c749fcf57ca505bd20050dcb4f78
Junlys3 <125740363+Junlys3@users.noreply.github.com>
2023-11-25 16:37:22 -0300

    t

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let menulist = window.document.querySelector(".menu-list")
 function entrou(){
     
     menulist.style.transform = 'translateX(0)'
+   
+    if(menulist.style.transform == 'translateX(0)'){
+        console.log("oi")
+    }
 
 }
 
-function saiu(){
-    menulist.style.transform = 'translateX(100%)'
-}
+
 
