3e3d70b822e6bf8b7bdc9ca8c2af66338c57f4c8
Junlys3 <125740363+Junlys3@users.noreply.github.com>
2023-11-27 12:35:35 -0300

    Finally

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 
+let Micon = document.querySelector(".menu-icon")
 
+let menu = document.querySelector(".menu-list")
 
+let closeicon = document.querySelector(".close")
+
+function abrir(){
+
+menu.style.transform = "translate(0,0)"
+
+    
+
+}
+
+function fechar(){
+    menu.style.transform = "translate(0,-110%)"
+}
