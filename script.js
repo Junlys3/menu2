2eb2740ab1d1f129ed328a7ee1bce3dca5354da2
Junlys3 <125740363+Junlys3@users.noreply.github.com>
2023-11-29 11:08:09 -0300

    testes

--- script.js
+++ script.js
@@ -6,32 +6,20 @@ let menu = document.querySelector(".menu-list")
 let closeicon = document.querySelector(".close")
 
 
-let li = document.querySelector(".listitem")
 
-let li1 = document.querySelector(".listitem1")
-
-let li2 = document.querySelector(".listitem2")
-
-let li3 = document.querySelector(".listitem3")
 
 
 function abrir(){
 
-menu.style.transform = "translate(0,0)"
+menu.style.opacity = "1"
 
     
 
 }
 
 function fechar(){
-    menu.style.transform = "translate(0,100%)"
- 
-    li.style.transform = "translate(0,-90%)"
+    menu.style.opacity = "0"
 
-    li1.style.transform = "translate(0,-90%)"
 
-    li2.style.transform = "translate(0,-90%)"
-
-    li3.style.transform = "translate(0,-90%)"
     
 }
